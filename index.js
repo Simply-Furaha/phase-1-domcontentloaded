dea1bfef566b623abd7f8daec2657ad901165bef
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-07-08 12:04:10 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    const targetElement = document.getElementById('text');
+    
+    targetElement.textContent = 'This is really cool!';
+  });
+  
